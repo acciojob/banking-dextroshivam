dcaa016297004bfad17843439a3c1001b92154f4
shivam <shivamp9148@gmail.com>
2023-06-17 13:59:37 +0530

    c1

--- src/main/java/com/driver/BankAccount.java
+++ src/main/java/com/driver/BankAccount.java
@@ -7,25 +7,57 @@ public class BankAccount {
     private double minBalance;
 
     public BankAccount(String name, double balance, double minBalance) {
+        this.name=name;
+        this.balance=balance;
+        this.minBalance=minBalance;
+    }
 
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
     }
 
     public String generateAccountNumber(int digits, int sum) throws Exception{
         //Each digit of an account number can lie between 0 and 9 (both inclusive)
         //Generate account number having given number of 'digits' such that the sum of digits is equal to 'sum'
         //If it is not possible, throw "Account Number can not be generated" exception
-
+        String ans="";
+        int start=(int)Math.pow(10,digits-1);
+        int end=(int)Math.pow(10,digits) -1;
+        for(int i=start;i<=end;i++){
+            if(checkSum(i,sum)) {
+              ans=i+"";
+              return ans;
+            }
+        }
+        if(ans.equals("")) throw new Exception("Account Number can not be generated");
         return null;
+    }
+
+    private boolean checkSum(int i,int sum) {
+        int curSum=0;
+        while (i != 0) {
+            curSum+=i%10;
+            i=i/10;
+        }
+        if(curSum==sum) return true;
+        else return false;
     }
 
     public void deposit(double amount) {
         //add amount to balance
-
+        this.balance+=amount;
     }
 
     public void withdraw(double amount) throws Exception {
         // Remember to throw "Insufficient Balance" exception, if the remaining amount would be less than minimum balance
-
+        if(balance<amount) throw new Exception ("Insuffiecient Balance");
+        else {
+            balance-=amount;
+        }
     }
 
 }
--- src/main/java/com/driver/CurrentAccount.java
+++ src/main/java/com/driver/CurrentAccount.java
@@ -4,16 +4,57 @@ public class CurrentAccount extends BankAccount{
     String tradeLicenseId; //consists of Uppercase English characters only
 
     public CurrentAccount(String name, double balance, String tradeLicenseId) throws Exception {
+        super(name, balance, 5000);
+        if (balance < 5000) throw new Exception("Insufficient Balance");
+        else{
+            this.tradeLicenseId=tradeLicenseId;
+        }
         // minimum balance is 5000 by default. If balance is less than 5000, throw "Insufficient Balance" exception
 
     }
 
+    private boolean isValid(String id) {
+        if(id.equals("")) return false;
+        for(int i=0;i<id.length()-1;i++){
+            if(id.charAt(i)==id.charAt(i+1)) return false;
+        }
+        return true;
+    }
     public void validateLicenseId() throws Exception {
         // A trade license Id is said to be valid if no two consecutive characters are same
         // If the license Id is valid, do nothing
         // If the characters of the license Id can be rearranged to create any valid license Id
         // If it is not possible, throw "Valid License can not be generated" Exception
+        boolean check = isValid(tradeLicenseId);
+        if(!check){
+            String s=tradeLicenseId;
+            int m[]=new int[26];//Hashmap to store the frequency of each character
+            int n=s.length();
+            for(int i=0;i<n;i++)
+                m[s.charAt(i)-'a']++;
 
+            StringBuilder ans=new StringBuilder();
+            int i=0;
+            char prev='*';//Keep track of previous character
+            while(i<n){
+                int maxi=0,ind=0;
+                //We traverse the entire hashmap to find the character with maximum count and not equal to previous character
+                for(int j=0;j<26;j++){
+                    if(m[j]>maxi&&prev!=(j+'a')&&m[j]>0){
+                        maxi=m[j];
+                        ind=j;
+                    }
+                }
+                if(maxi==0) {
+                    throw new Exception("Valid License can not be generated");
+                };//If maxi is 0 then no solution can be made as suitable element not found
+                prev=(char)(ind+'a');//Updating previous
+                ans.append(prev);
+                m[ind]--;  //Decreasing count from map since the character is done
+                i++;
+            }
+            tradeLicenseId=String.valueOf(ans);
+        }
     }
 
 }
